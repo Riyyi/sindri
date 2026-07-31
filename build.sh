a012160f724547a93800ce5c2a9197d322d85dc8
Riyyi <riyyi3@gmail.com>
2026-07-31 20:37:14 +0200

    Make build script valid POSIX

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 PROJECT="sindri"
 VERSION="dev-$(date -u '+%Y-%m-%d')-$(git rev-parse --short HEAD)"
 
-if [[ $1 == "debug" ]]
-then
+if [ "$1" = "debug" ]; then
     shift
 
-    odin build src/ -show-timings -collection:src=src -out:$PROJECT -microarch:native -use-separate-modules -define:VERSION=$VERSION-debug -debug $@
+    odin build src/ -show-timings -collection:src=src -out:$PROJECT -microarch:native -use-separate-modules -define:VERSION="$VERSION-debug" -debug "$@"
     exit 0
 fi
 
-odin build src/ -show-timings -collection:src=src -out:$PROJECT -microarch:native -o:speed -define:VERSION=$VERSION $@
+odin build src/ -show-timings -collection:src=src -out:$PROJECT -microarch:native -o:speed -define:VERSION="$VERSION" "$@"
